a30c671012367e1a0102f197811100b046d65f96
famgz <famgz@proton.me>
2024-04-24 18:36:45 -0300

    feat: header dynamic date elements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  today = new Date();
 
+  getFormattedDate(): string {
+    const formattedDate = this.today.toLocaleString('pt-br', {
+      weekday: 'long',
+      day: '2-digit',
+      month: 'long',
+    });
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  }
+
+  getFormattedHour(): string {
+    return this.today.toLocaleString('pt-br', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.today = new Date();
+    }, 1000);
+  }
 }
